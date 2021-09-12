03368652ecd1ef05f48ab52bf6e73422778e5328
Nicolas Brousse <n.brousse@pantographe.studio>
2021-09-12 03:27:10 +0200

    wip

--- spec/internal/db/schema.rb
+++ spec/internal/db/schema.rb
@@ -3,4 +3,9 @@
 ActiveRecord::Schema.define do
   # Set up any tables you need to exist for your test suite that don't belong
   # in migrations.
+  create_table(:users, force: true) do |t|
+    t.string :first_name
+    t.string :last_name
+    t.timestamps
+  end
 end
